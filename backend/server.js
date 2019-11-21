f795ee72e40987bc74de79779173d5d6fcac7dc7
Victor Okpon <vikzy2rhyme@yahoo.com>
2019-11-21 11:15:52 +0100

    Setup eslint for linting and airbnb style guide.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-  res.end('Teamwork backend local server response.');
+app.set('port', process.env.PORT || 3000)
+const server = http.createServer(app);
+
+app.use((req, res, next) => {
+  res.json({
+    message: 'app served'
+  });
 });
 
 server.listen(process.env.PORT || 3000);
