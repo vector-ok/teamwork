9526fe2c0c0b6d50dfbf716256335355308cb573
Victor Okpon <vikzy2rhyme@yahoo.com>
2019-11-28 22:44:56 +0100

    Added signup endpoint. Installed bodyParser.

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const { Pool } = require('pg');
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 const pool = new Pool({
 
   user: 'admin', // this is the db user credential
@@ -20,6 +24,45 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
+});
+
+// POST /auth
+app.post('/auth/create-user', (req, res) => {
+  bcrypt.hash(req.body.password, 10).then(
+    (hash) => {
+      const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hash,
+        gender: req.body.gender,
+        jobRole: req.body.jobRole,
+        department: req.body.department,
+        address: req.body.address,
+        userId: req.body.userId,
+      };
+
+      pool.connect((err, client, done) => {
+        const query = 'INSERT INTO employees(firstName, lastName, email, password, gender, imageUrl, jobRole, department, address, userId) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING *';
+        const values = [user.firstName, user.lastName,
+          user.email, user.password, user.gender,
+          user.imageUrl, user.jobRole, user.department, user.address, user.userId];
+
+        client.query(query, values, (error) => {
+          done();
+          if (error) {
+            throw error;
+          } else {
+            res.status(201).json({
+              status: 'success',
+              message: 'User account successfully created',
+              info: user,
+            });
+          }
+        });
+      });
+    },
+  );
 });
 
 
