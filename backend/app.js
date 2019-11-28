b8b12c7a8be0d138f3152cea1dd950f9eaa14dea
Victor Okpon <vikzy2rhyme@yahoo.com>
2019-11-28 18:23:47 +0100

    Setup Database and required tables. Handled CORS error on local Database. Installed missing dependencies.

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,34 @@ const express = require('express');
 
 const app = express();
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+
+  user: 'admin', // this is the db user credential
+  host: 'localhost',
+  database: 'teamwork_db',
+  password: 'admin',
+  port: 5432,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000,
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+// **********************  ***********
+
+app.use('/', (req, res, next) => {
+  res.json({
+    message: 'app served successfully. THis is a default route response!',
+  });
+  next();
+});
+
 module.exports = app;
