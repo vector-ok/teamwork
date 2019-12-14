b8284989cdc0b16511a9f74bc7a10ad7fbf2b6b5
Victor Okpon <vikzy2rhyme@yahoo.com>
2019-12-14 02:07:39 +0100

    Updated gifs post controller to integrate cloudinary image upload.

--- backend/controllers/gifController.js
+++ backend/controllers/gifController.js
@@ -1,49 +1,90 @@
-const pool = require('../services/config');
+const express = require('express');
 
-// POST gif
+const app = express();
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+const cloudinaryConfig = require('../services/config');
+const poolConfig = require('../services/config');
+
+const { cloudIt } = cloudinaryConfig;
+const { pool } = poolConfig;
+
+const storageHandle = (req, file, cb) => {
+  cb(null, 'controllers/uploads');
+};
+const fileHandle = (req, file, cb) => {
+  console.log(file);
+  cb(null, file.originalname);
+};
+const storage = multer.diskStorage({
+  destination: storageHandle,
+  filename: fileHandle,
+});
+
 const gifPost = (req, res) => {
-  const data = {
-    title: req.body.title,
-    imageUrl: req.body.imageUrl,
-  };
+  const upload = multer({ storage }).single('images');
+  upload(req, res, (err) => {
+    if (err) {
+      return res.send(err);
+    }
+    //  return res.json(req.file); // file uploaded to server
+    console.log('file uploaded to server');
+    console.log(req.file);
 
-  pool.connect((err, client, done) => {
-    const query = 'INSERT INTO gifs(title, imageUrl) VALUES($1,$2) RETURNING *';
-    const values = [data.title, data.imageUrl];
-
-    client.query(query, values, (error) => {
-      done();
-      if (error) {
-        throw error;
-      } else {
-        res.status(201).json({
-          status: 'success',
-          message: 'gif added successfully',
-          info: data,
-        });
-      }
+    // Send file to Cloudinary
+    cloudinary.config({
+      cloud_name: cloudIt.CLOUD_NAME,
+      api_key: cloudIt.API_KEY,
+      api_secret: cloudIt.API_SECRET,
     });
+    const { path } = req.file;
+    const uniqueFilename = new Date().toISOString();
+
+    cloudinary.uploader.upload(
+      path,
+      { puplic_id: `gifs/${uniqueFilename}`, tags: 'gifs' },
+      (error, image) => {
+        if (error) return res.send(error);
+        console.log('file uploaded to cloudinary');
+
+        // remove file from server
+        fs.unlinkSync(path);
+
+        pool.query('INSERT INTO gifs(title, imageUrl) VALUES($1,$2) RETURNING *', [image.original_filename, image.url]);
+
+        if (error) {
+          throw error;
+        }
+        return res.status(201).json(image);
+
+        // return image details
+      },
+    );
+    return console.log('post successful');
   });
-  // next();
 };
 
 // DELETE gif
-const gifDelete = (req, res) => {
-  pool.connect((err, client, done) => {
-    const query = 'DELETE FROM gifs WHERE id = $1';
-    client.query(query, [req.params.id], (error) => {
-      done();
-      if (!err) {
-        res.status(200).json({
-          status: 'success',
-          message: 'gif post successfully deleted',
-        });
-      } else {
-        res.status(400).json({ error });
-      }
+
+const gifDelete = (async (req, res, next) => {
+  try {
+    const result = await pool.query('DELETE FROM gifs WHERE id = $1', [req.params.id]);
+
+    if (result.rowCount < '1') {
+      return res.status(400).json({
+        message: 'record not found!',
+      });
+    }
+    return res.status(200).json({
+      status: 'success',
+      message: 'gif post successfully deleted',
     });
-  });
-};
+  } catch (error) {
+    console.error(error);
+  }
+  return next();
+});
 
 const commentPost = (req, res) => {
   const data = {
@@ -70,3 +111,5 @@ const commentPost = (req, res) => {
 };
 
 module.exports = { gifPost, gifDelete, commentPost };
+
+app.listen(7000);
